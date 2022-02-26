8a33f02427ee0decefbbd7078a14b6744d0bd419
Sumner Bradley <sumnerjbradley@gmail.com>
2022-02-26 14:44:18 -0700

    Add tcp client

---
+++ tcp/client/client.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	c, err := net.Dial("tcp", "localhost:8090")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(">>")
+		text, _ := reader.ReadString('\n')
+		fmt.Fprintf(c, text+"\n")
+
+		message, _ := bufio.NewReader(c).ReadString('\n')
+		fmt.Print("->: " + message)
+		if strings.TrimSpace(string(text)) == "STOP" {
+			fmt.Println("TCP client exiting...")
+			return
+		}
+	}
+}
